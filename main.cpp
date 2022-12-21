4a308d286215faa6fe13a71971977b30787fc23c
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-21 20:51:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,14 @@
 using namespace std;
 
 double f (float num) {
-    double fx = (pow(num, 2) * cos(num) + 1);
-    return fx;
+    num = (pow(num, 2) * cos(num) + 1);
+    return num;
 }
 
 int main() {
 
     float a=0, b=0, x, err=1;
 
-    cout << "inserire estremi";
-    cin >> a >> b;
 
     while (f(a)*f(b)>=0) {
         cout << "inserire estremi";
@@ -23,25 +21,23 @@ int main() {
         x = (a + b) / 2;
 
         if (f(x) == 0) {
-            x = x*10000;
-            x =(int)x;
-            x = x/10000;
-            cout << x;
+            break;
 
         } else {
             if (f(a) * f(b) < 0) {
                 b = x;
             } else a = x;
 
-            err = abs((b - a) / 2);
         }
+            err = abs((b - a) / 2);
+        
     } while (err >= 1e-6);
 
     x = x*10000;
     x =(int)x;
     x = x/10000;
     cout << x;
-    
-    
+
+
     return 0;
 }
