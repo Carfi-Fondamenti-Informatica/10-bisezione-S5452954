582dacace8a6ff85d62f0d05c3c2dee64bbf1bfc
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-21 21:04:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
-#include "cmath"
+#include <cmath>
 using namespace std;
 
-double f (float num) {
-    num = (pow(num, 2) * cos(num) + 1);
-    return num;
+float f( float a){
+    float y=0;
+    y= ((a*a)* cos(a))+1;
+    return y;
 }
 
 int main() {
-
-    float a, b, x, err;
-
-
-    while (f(a)*f(b)>=0) {
-        cout << "inserire estremi";
-        cin >> a >> b;
+    float a=0, b=0, m=0,err=1;
+    while((f(a)* f(b))>=0){
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
     }
 
-    do {
-        x = (a + b) / 2;
-
-        if (f(x) == 0) {
-            break;
-
-        } else {
-            if (f(a) * f(b) < 0) {
-                a = x;
-            } else b = x;
-
+    while (f(m)!=0){
+        m=((a+b)/2);
+        if ((f(m)*f(b))<0){
+            a=m;
+        }else{
+            b=m;
         }
-            err = (b - a) / 2;
-        
-    } while (err >= 1e-6);
+        err=((b-a)/2);
+        if (err<(1e-6)){
+            break;
+        }
+    }
 
-    x = x*10000;
-    x =(int)x;
-    x = x/10000;
-    cout << x;
+    m=m*10000;
+    m=(int)m;
+    m=m/10000;
+    cout<< m;
 
 
     return 0;
